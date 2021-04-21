e7015a9998fadb1d5eb1443c0699d392bc18ee67
dumdump <61518949+dumdump@users.noreply.github.com>
2021-04-21 10:59:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,58 @@
+//variables
+//game
 var whichKeyPressed;
-var buttonE = document.getElementById("e-button");
-var buttonQ = document.getElementById('q-button');
+var buttonE = document.getElementById('e-button');;
+var buttonQ;
 var gameCanvas;
 var game;
+var timerScore = 0;
+var baseHealth = 100;
+var difficulty = 1;
+var baseHealthEl = document.getElementById('base-health');
+var timerScoreEl = document.getElementById('timer-score');
+var difficultyEl = document.getElementById('level');
+//chracter
 var speed = 10;
-var bulletSpeed = 50;
+var bulletSpeed = 10;
 var characterPosX = 0;
 var characterPosY = 0;
+var characterDimensionW;
+var characterDimensionH;
 var bulletPosX = 0;
 var bulletPosY = 0;
 var shotAvailable = true;
+var shooting = false;
+//zombie
+var zombieSpeed;
+var zombieDimemsionW;
+var zombieDimensionH;
+var zombiePosX;
+var zombiePosY;
+var zombieHit = false;
+var zombieIsShot = false;
+var zombieHitStructure = false;
+var zombieScore = 0;
 
 const pause = time => new Promise(resolve => setTimeout(resolve, time))
 
+//constructers
+function Character(speed, bulletSpeed, width, height) {
+  this.speed = speed;
+  this.bulletSpeed = bulletSpeed;
+  this.width = characterDimensionW;
+  this.height = characterDimenionH;
+}
+function Zombie(speed, width, height, posX, posY) {
+  this.speed = speed;
+  this.width = width;
+  this.height = height;
+  this.posX = posX;
+  this.posY = posY;
+}
+function RandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+//functions
 function draw() {
   gameCanvas = document.getElementById('game');
   game = gameCanvas.getContext('2d');
@@ -24,6 +64,64 @@ function draw() {
 
   game.fillStyle = "gray";
   game.fillRect(0, 0, 40, gameCanvas.height);
+
+  setTimeout(startGameTimer, 1000);
+  setTimeout(spawnZombie, 1000);
+}
+
+function createZombie() {
+  newZombie = new Zombie(RandomNumber(50), 40, 40, 700, RandomNumber(200)+50);
+}
+
+async function spawnZombie() {
+  zombieHit = false;
+  createZombie();
+  game.fillStyle = "green";
+  zombiePosX = newZombie.posX;
+  zombiePosY = newZombie.posY;
+  zombieDimensionW = newZombie.width;
+  zombieDimensionH = newZombie.height;
+  zombieSpeed = newZombie.speed;
+  game.fillRect(zombiePosX, zombiePosY, zombieDimensionW, zombieDimensionH);
+  while (zombiePosX > 40 && zombieHit == false) {
+    testZombieCollision();
+    game.clearRect(zombiePosX, zombiePosY, zombieDimensionW, zombieDimensionH);
+    zombiePosX -= difficulty*2;
+    game.fillStyle = "green";
+    game.fillRect(zombiePosX, zombiePosY, zombieDimensionW, zombieDimensionH);
+    await pause(zombieSpeed);
+  }
+  await pause(500);
+  game.clearRect(zombiePosX, zombiePosY, zombieDimensionW, zombieDimensionH);
+  zombieLoop();
+}
+
+function zombieLoop() {
+  game.fillStyle = "gray";
+  game.fillRect(0, 0, 40, gameCanvas.height);
+  console.log(zombiePosX);
+  if (zombiePosX == 40) {
+    baseHealth -= RandomNumber(25);
+    baseHealthEl.innerHTML = "McPeng Sports Health: " + baseHealth + "%";
+  }
+  if (zombieScore == 10) {
+    difficulty++;
+    zombieScore = 0;
+    difficultyEl.innerHTML = "Level: " + difficulty;
+  }
+  if (baseHealth <= 0) {
+    alert("McPeng Sports has fallen... " + "Time Alive: " + timerScore);
+  } else {
+    spawnZombie();
+  }
+}
+
+function testZombieCollision() {
+  var zombieCoordsYMax = zombiePosY + zombieDimensionH;
+  if (bulletPosY > zombiePosY && bulletPosY < zombieCoordsYMax && characterPosX < zombiePosX && shooting == true) {
+    zombieHit = true;
+    zombieScore++;
+  }
 }
 
 function help() {
@@ -31,7 +129,6 @@ function help() {
 }
 
 function leftKeyPressed() {
-  console.log("left key is pressed.");
   if (characterPosX > 70) {
     game.fillStyle = "red";
     game.clearRect(characterPosX + speed, characterPosY, 50, 50);
@@ -41,7 +138,6 @@ function leftKeyPressed() {
 }
 
 function upKeyPressed() {
-  console.log("up key is pressed.");
   if (characterPosY > 0) {
     game.fillStyle = "red";
     game.clearRect(characterPosX, characterPosY + speed, 50, 50);
@@ -51,7 +147,6 @@ function upKeyPressed() {
 }
 
 function rightKeyPressed() {
-  console.log("right key is pressed.");
   game.fillStyle = "red";
   game.clearRect(characterPosX - speed, characterPosY, 50, 50);
   characterPosX += speed;
@@ -59,7 +154,6 @@ function rightKeyPressed() {
 }
 
 function downKeyPressed() {
-  console.log("down key is pressed.");
   if (characterPosY < 250) {
     game.fillStyle = "red";
     game.clearRect(characterPosX, characterPosY - speed, 50, 50);
@@ -69,25 +163,24 @@ function downKeyPressed() {
 }
 
 async function eKeyPressed() {
+  shooting = true;
   shotAvailable = false;
-  console.log("e key is pressed");
   game.fillStyle = "orange";
   bulletPosX = characterPosX + 50;
   bulletPosY = characterPosY + 25;
   game.fillRect(bulletPosX, bulletPosY, 20, 5);
   while (bulletPosX < 1000) {
     game.clearRect(bulletPosX, bulletPosY, 20, 5);
-    bulletPosX += bulletSpeed;
+    bulletPosX += 20;
     game.fillStyle = "orange";
     game.fillRect(bulletPosX, bulletPosY, 20, 5);
-    await pause(50).then(console.log('jaklsdfjlkasdfj'));
-    console.log(bulletPosX);
+    await pause(bulletSpeed);
   }
   shotAvailable = true;
+  shooting = false;
 }
 
 function qKeyPressed() {
-  console.log("q key is pressed");
 }
 
 function keyPressed(e) {
@@ -107,10 +200,22 @@ function keyPressed(e) {
 }
 
 function buttonEPressed() {
+  console.log("e button pressed")
   if (shotAvailable == true) {
     eKeyPressed();
   }
 }
 
+function startGameTimer() {
+  setInterval(function() {
+    timerScore += 1;
+    timerScoreEl.innerHTML = "Time Alive: " + timerScore;
+    console.log(timerScore);
+  }, 1000)
+}
+
+//listeners
 document.addEventListener('keypress', keyPressed);
-console.log(buttonE);
+buttonE.addEventListener('click', buttonEPressed);
+baseHealthEl.innerHTML = "McPeng Sports Health: 100%";
+difficultyEl.innerHTML = "Level: 1";
